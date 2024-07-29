4cd54f512e15e93515ca18a1cc3e427a0364e221
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-07-29 12:31:07 +0330

    Removed unused lines

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from os import system
+
 print("Hi !")
 
-def install() -> None:
+
+def main() -> None:
     print("Installing pipenv...")
     system("pip install pipenv")
     print("Pipenv installed successfully.")
+    system("mkdir {name}")
+    system("cd {name}")
+    with open("main.py", "w") as f:
+        f.write(
+            "from flask import Flask\napp = Flask(__name__)\n\n@app.route('/')\ndef index():\n    return 'Hello, World!'\n\nif __name__ == '__main__':\n    app.run(debug=True)"
+        )
     print("Creating virtual environment...")
     system("pipenv shell")
     print("Virtual environment created successfully.")
     print("Installing requirements...")
     system("pipenv install flask")
     print("Requirements installed successfully.")
-
-
-def main():
-    install()
     print("Installing is succesfully done!")
     print("Creating a new Flask app...")
     name = input("Enter the name of the app: ")
-    system("mkdir {name}")
-    system("cd {name}")
-    with open("main.py", "w") as f:
-        f.write("from flask import Flask\napp = Flask(__name__)\n\n@app.route('/')\ndef index():\n    return 'Hello, World!'\n\nif __name__ == '__main__':\n    app.run(debug=True)")
     print("Flask app created successfully.")
     print("Flask app is ready to use!")
     print("You can now run the app using the command 'pipenv run python main.py'")
